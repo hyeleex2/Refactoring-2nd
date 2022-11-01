b5b4344cf47c5fb042e108ab81f8f60526fc3f48
OPENGA_Dev_hllee-at-900616242684 <hl.lee@quintet.co.kr>
2022-11-01 13:36:58 +0900

    다형성 서브 클래스 추가

---
+++ ch1/createStatementData.js
@@ -0,0 +1,81 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+
+  // 계산 로직
+  get amount() { 
+    throw new Error(`서브 클래스에서 처리하도록 설계됨`)
+  }
+
+  get volumnCredits () {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    if('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  
+  class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+      let result = 40000;
+      if(this.performance.audience > 30) {
+        result += 1000 * (this.performance.audience - 30);
+      }
+      return result;
+    }
+  }
+
+  class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+      let result = 30000;
+      if(this.performance.audience > 20) {
+        result += 10000 + 500 * (this.performance.audience - 20);
+      }
+      result += 300 * this.performance.audience;
+      return result;
+    }
+  }
+
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default: 
+      throw new Error(`알 수 없는 장르 ${aPlay.type}`)
+  }
+}
+
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance)
+  result.totalAmount = totalAmount(result)
+  result.volumnCredits = totalVolumnCredits(result)
+  return result
+
+  function enrichPerformance(aPerformance) {
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumnCredits = calculator.volumnCredits;
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playId];
+  }
+
+  function totalAmount(data) {
+    return data.performances
+      .reduce((total, p) => total + p.amount, 0);
+  }
+
+  function totalVolumnCredits(data) {
+    return data.performances
+      .reduce((total, p) => total + p.volumnCredits, 0);
+  }
+}
